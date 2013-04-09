923a3a13853d67860190d030ec0a033d35d8cde6
usrbincc <usrbincc@yahoo.com>
2013-04-09 10:19:34 -0400

    Combine all 'Fixes" directives with the final "Closes" line.

--- pulley.js
+++ pulley.js
@@ -224,6 +224,7 @@
 				author = JSON.parse( data )[ 0 ].commit.author.name,
 				base_branch = pull.base.ref,
 				issues = [],
+				issuesLine = '',
 				urls = [],
 				findBug = /[Ff]ixes #(\d+)/g;
 
@@ -236,11 +237,13 @@
 
 			// Search just body for issues to add to the commit message
 			while ( ( match = findBug.exec( pull.body ) ) ) {
-				issues.push( " Fixes #" + match[ 1 ] );
+				issues.push( "fixes #" + match[ 1 ] );
 			}
 
 			// Add issues to the commit message
-			msg += issues.join(",");
+			if (issues.length) {
+				issuesLine = ", " + issues.join(", ");
+			}
 
 			if ( urls.length ) {
 				msg += "\n\nMore Details:" + urls.map(function( url ) {
@@ -250,7 +253,7 @@
 
 			msg += '\n\n' + pull.body;
 			msg += '\n\nReview URL: ' + pull.issue_url;
-			msg += '\n\nCloses #' + id;
+			msg += '\n\nCloses #' + id + issuesLine;
 
 			var commit = [ "commit", "-a", "--message=" + msg ];
 
